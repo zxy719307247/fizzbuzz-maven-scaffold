3820c959f6cce9d0af9daa6ad09dd567185fdd2c
zxy719307247 <15620743110@163.com>
2019-12-28 11:51:59 +0800

    pass

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -12,7 +12,9 @@ public class FizzBuzz {
 
 
     public static String of(int number) {
-        
+        if(number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }
         if(number % 3 == 0){
             return "Fizz";
         }
