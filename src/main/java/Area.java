7c05229c55d4c89a776ca70b517cd5d3274b07dd
zxy719307247 <15620743110@163.com>
2019-12-28 16:41:38 +0800

    refactor

--- src/main/java/Area.java
+++ src/main/java/Area.java
@@ -4,11 +4,8 @@
  * @date: 2019/12/28
  */
 public class Area {
-    private int x;//x坐标值
-    private int y;//y坐标值
-//    private String fx;//方向
-//    static int Max_x;//最大x值
-//    static int Max_y;//最大y值
+    private int x;
+    private int y;
 
     public Area(int x, int y) {
         this.x = x;
@@ -19,15 +16,7 @@ public class Area {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public int getY() {
         return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 }
